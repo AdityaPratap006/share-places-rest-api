d73f3dd3e355e1bcc82727b98a7a386f594ce8ef
Aditya Pratap <adityapratap7344@gmail.com>
2020-08-12 17:39:02 +0530

    Created a basic express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello world');
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send({
+        message: 'Hi there!',
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+});
