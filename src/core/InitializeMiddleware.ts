44daaccf2a0a49b1f8d632636e851f2cc974f9d6
Aditya Pratap <adityapratap7344@gmail.com>
2020-08-12 19:50:36 +0530

    Initializing middlewares

---
+++ src/core/InitializeMiddleware.ts
@@ -0,0 +1,20 @@
+import { Express } from 'express';
+import { CommonMiddleware, ErrorHandlingMiddleware } from '../middleware';
+
+export class InitializeMiddleware {
+
+    public static async InitializeCommonMiddleware(app: Express) {
+        const middleware = new CommonMiddleware(app);
+
+        await middleware.useBodyParser();
+        await middleware.useURLencoded();
+        await middleware.useCors();
+    }
+
+    public static async InitializeErrorHandlingMiddleware(app: Express) {
+        const errorMiddleware = new ErrorHandlingMiddleware(app);
+
+        await errorMiddleware.handle404Error();
+        await errorMiddleware.handle500Error();
+    }
+}
